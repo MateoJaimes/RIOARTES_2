f0d55a71181c0319b00c60a53b02ad446411cdfe
MateoJaimes <mateojaime201@gmail.com>
2022-11-14 18:47:10 -0500

    ACTUALIZACIONES JS

--- js/funtion_reloj.js
+++ js/funtion_reloj.js
@@ -1,23 +1,28 @@
-$(document).ready(function inicio_reloj() {
-    var today = new Date();
-    var hr = today.getHours();
-    var min = today.getMinutes();
-    var sec = today.getSeconds();
-    ap = (hr < 12) ? "<span>A.M</span>" : "<span>P.M</span>";
-    hr = (hr == 0) ? hr - 12 : hr;
-    hr = checkTime(hr);
-    min = checkTime(min);
-    sec = checkTime(sec);
-    hr = $("#hora").val();
-    min = $("#minutos").val();
-    sec = $("#segundos").val();
-    ap = $("#ampm").val();
-    var tiempo = setTimeout(function(){inicio_reloj()},500);
-    console.log(hr," ",min," ",sec)
-});
-function checkTime(i){
-    if(i < 10){
-        i = "0" + i;
+var hr = document.querySelector('.hora');
+var min = document.querySelector('.minutos');
+var seg = document.querySelector('.segundos');
+var ap = document.querySelector('.ampm')
+
+const actualizar_hora = setInterval(function(){
+    var date = new Date();
+    let horas = date.getHours();
+    let minutos = date.getMinutes();
+    let segundos = date.getSeconds();
+    let ampm;
+    
+    if (horas < 12){
+        ampm = "AM";
+    }else{
+        ampm = "PM";
     }
-    return i; 
-}
+    
+    if (horas > 12){
+        horas = horas - 12;
+    }
+    
+
+    hr.innerHTML = horas;
+    min.innerHTML = minutos;
+    seg.innerHTML = segundos;
+    ap.innerHTML = ampm;
+},1000);
